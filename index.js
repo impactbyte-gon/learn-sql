3624c6c94be40e6a1184b103db4cb2196fa67f12
M Haidar Hanif <git@mhaidarhanif.com>
2019-02-21 17:41:16 +0700

    Use env

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+require('dotenv').config()
+
 const knex = require('knex')({
   client: 'mysql2',
   connection: {
-    host: 'localhost',
-    user: 'root',
-    password: 'helloworld',
-    database: 'mytasks'
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'yourpassword',
+    database: process.env.DB_DATABASE || 'yourdatabasename'
   }
 })
 
