5dcdeeb2d4080c9bc49e975876428f61b8af65bc
M Haidar Hanif <git@mhaidarhanif.com>
2019-02-21 17:38:40 +0700

    Use knex select

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const knex = require('knex')({
-  client: 'mysql',
+  client: 'mysql2',
   connection: {
     host: 'localhost',
     user: 'root',
@@ -8,4 +8,6 @@ const knex = require('knex')({
   }
 })
 
-console.log(knex)
+knex('users').then(rows => {
+  console.log(rows)
+})
